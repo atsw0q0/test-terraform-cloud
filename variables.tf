319a6075b05b1271ed8e020c5ddf2e73c6184a1f
neko-kick <n26kdgrat@gmail.com>
2024-09-15 12:52:40 +0900

    add igw

--- variables.tf
+++ variables.tf
@@ -31,3 +31,38 @@ variable "vpc_prefix" {
     cidr_block = "10.0.0.0/16"
   }
 }
+
+# variable "subnet" {
+#     type = optional(map(object({ 
+#         prefix = string
+#         cidr_block = string
+#         az = string
+#         is_public_subnet = bool
+#     })))
+#     default = {
+#         pub_a = {
+#             prefix = "pub"
+#             cidr_block = "10.0.0.0/24"
+#             az = "a"
+#             is_public_subnet = true
+#         }
+#         pub_c = {
+#             prefix = "pub"
+#             cidr_block = "10.0.1.0/24"
+#             az = "c"
+#             is_public_subnet = true
+#         }
+#         pri_a = {
+#             prefix = "pri"
+#             cidr_block = "10.0.10.0/24"
+#             az = "a"
+#             is_public_subnet = false
+#         }
+#         pri_c = {
+#             prefix = "pri"
+#             cidr_block = "10.0.11.0/24"
+#             az = "c"
+#             is_public_subnet = false
+#         }
+#     }
+# }
