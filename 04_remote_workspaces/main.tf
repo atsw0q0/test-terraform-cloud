51ecedd7c3c94efed4d040dc4156bc9994436c7a
neko-kick <n26kdgrat@gmail.com>
2024-09-24 09:47:09 +0900

    Add EC2 Instance nonsensitive_values

--- 04_remote_workspaces/main.tf
+++ 04_remote_workspaces/main.tf
@@ -13,9 +13,9 @@ module "ec2_instance" {
   ec2 = {
     prefix        = "webapp"
     instance_type = "t4g.micro"
-    subnet_id     = data.tfe_outputs.vpc.values.network-templates_values.subnet_pub_ids.pub_a
+    subnet_id     = data.tfe_outputs.vpc.nonsensitive_values.network-templates_values.subnet_pub_ids.pub_a
     vpc_security_group_ids = [
-      data.tfe_outputs.vpc.values.ec2-sg_values.sg_id
+      data.tfe_outputs.vpc.nonsensitive_values.ec2-sg_values.sg_id
     ]
     is_pubic = true
   }
